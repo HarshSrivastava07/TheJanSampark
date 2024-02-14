c1ff4c3cb8ea26cc68096fbb7661fccdf37c2e22
Ashish Raj <ashi1110000111@gmail.com>
2024-02-14 22:15:30 +0530

    chatBot integrated and project restructuring

--- agency/src/DataBase/firebase.js
+++ agency/src/DataBase/firebase.js
@@ -5,6 +5,13 @@ import {getFirestore}  from "@firebase/firestore"
 
 const firebaseConfig = {
   //add here
+  apiKey: "AIzaSyDBx4nhqiGcy5JI9DEk3OBwksXLVfrt8us",
+  authDomain: "thejansampark-2e406.firebaseapp.com",
+  projectId: "thejansampark-2e406",
+  storageBucket: "thejansampark-2e406.appspot.com",
+  messagingSenderId: "225162803016",
+  appId: "1:225162803016:web:be1549f0fa2b5bb1cba57d",
+  measurementId: "G-V90WBT9P6C"
 };
 
 // Initialize Firebase
